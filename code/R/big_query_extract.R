02128a8fa8da4d31caa4b36e06302cc79e40b735
Saif Rais <saifrais@gmail.com>
2017-12-10 19:32:32 +0000

    Earth Engine Data Required

---
+++ code/R/big_query_extract.R
@@ -0,0 +1,20 @@
+#Ref: http://thinktostart.com/using-google-bigquery-with-r/
+
+#'@name get_table
+#'@description Get table details from BigQuery
+#'@param project
+#'@param tablename Options: nss72: district_map, districtwise_mpce,
+#'household_characteristics, household_expenditure
+#'@return Dataframe of table data
+#'@export
+#'@import bigrquery
+get_table <- function(tablename = 'nss72.districtwise_mpce',
+                      project = 'peerless-robot-187203'){
+  library(assertthat)
+  library(bigrquery)
+  #[peerless-robot-187203:nss72.districtwise_mpce]
+  sql_str <- sprintf('SELECT * FROM [%s:%s]', project, tablename)
+  data <- query_exec(query = sql_str, project = project)
+  return(data)
+  }
+
--- data/earthengine/README.md
+++ data/earthengine/README.md
@@ -1,1 +1,8 @@
 # Folder where python script extracts all geotiff files from Earth Engine API.
+The folder will contain monthly *.geotiff files from the following source:
+ - VIIRS Stray Light Corrected Nighttime Day/Night Band Version 1
+ - Date Range (monthly) 1-Jan-2014 to current
+ - Gamma : 2.0
+ - Naming convention: VIIRS_MM_YYYY.tiff
+ - In order to reduce the size of each exported file, please use only
+ the extent of the country under analysis (i.e. India)
