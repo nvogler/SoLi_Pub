8c7218a77af363b91698c700da29e2d254c07d8b
Saif Rais <saifrais@gmail.com>
2017-12-16 20:01:37 +0000

    Python from Shell

---
+++ code/python/python_auth.py
@@ -0,0 +1,9 @@
+import ee
+try:
+  ee.Initialize()
+  print('The Earth Engine package initialized successfully!')
+except ee.EEException as e:
+  print('The Earth Engine package failed to initialize!')
+except:
+    print("Unexpected error:", sys.exc_info()[0])
+    raise
